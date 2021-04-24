7d0c7945f7f69c6f7f337528faa17f70f07d4b40
Samuel Kojicovski <kojicovski@gmail.com>
2021-04-24 09:44:03 -0300

    ADD movielens.py: Exercicio 1 e 2

--- movielens-dataset-analysis/movielens.py
+++ movielens-dataset-analysis/movielens.py
@@ -3,15 +3,15 @@ import pandas as pd
 pd.options.display.max_rows = 10
 
 unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
-users = pd.read_table('datasets/movielens/users.dat', sep='::',
+users = pd.read_table('./datasets/movielens/users.dat', sep='::',
                       header=None, names=unames, engine='python')
 
 rnames = ['user_id', 'movie_id', 'rating', 'timestamp']
-ratings = pd.read_table('datasets/movielens/ratings.dat', sep='::',
+ratings = pd.read_table('./datasets/movielens/ratings.dat', sep='::',
                         header=None, names=rnames, engine='python')
 
 mnames = ['movie_id', 'title', 'genres']
-movies = pd.read_table('datasets/movielens/movies.dat', sep='::',
+movies = pd.read_table('./datasets/movielens/movies.dat', sep='::',
                        header=None, names=mnames, engine='python')
 data = pd.merge(pd.merge(ratings, users), movies)
 
@@ -49,14 +49,18 @@ rating_std_by_title.sort_values(ascending=False)[:10]
 
 
 # Os 10 filmes melhor classificados (com mais de 300 avaliações);
+ratings_by_title = data.groupby('title').size()
 ratings_by_title[:10]
 active_titles = ratings_by_title.index[ratings_by_title >= 300]
-active_titles
-
-max_ratings = max_ratings.loc[active_titles]
-max_ratings
+mean_ratings = mean_ratings.loc[active_titles]
+mean_ratings['total'] = mean_ratings['M'] + mean_ratings['F']
+top_ten_ratings = mean_ratings.sort_values(by='total', ascending=False)
+top_ten_ratings[:10]
 # Os 10 filmes pior classificados (com mais de 300 avaliações);
+down_ten_ratings = mean_ratings.sort_values(by='total', ascending=True)
+down_ten_ratings[:10]
 # Quantidade de filmes por gênero (considerar apenas o primeiro gênero de cada filme);
+
 # Os 10 filmes melhor classificados no gênero "comedy".
 
 
